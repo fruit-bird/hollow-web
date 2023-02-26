bb107988e17dc729d6723c445a0bb753dc9950db
fruit-bird <github.ru3w9@slmail.me>
2023-02-26 17:51:59 +0100

    does not work in wasm, suspect hollow repo itself

--- src/app.rs
+++ src/app.rs
@@ -1,26 +1,27 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
+use hollow::Prompt;
 
-    // this how you opt-out of serialization of a member
-    #[serde(skip)]
-    value: f32,
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+#[derive(serde::Deserialize, serde::Serialize, Clone)]
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+pub struct HollowApp {
+    // Example stuff:
+    normal_link: String,
+    conspiracy_link: String,
+    output: String,
 }
 
-impl Default for TemplateApp {
+impl Default for HollowApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            normal_link: "https://en.wikipedia.org/wiki/Rumpelstiltskin".to_owned(),
+            conspiracy_link: "https://en.wikipedia.org/wiki/Moon_landing_conspiracy_theories"
+                .to_owned(),
+            output: Default::default(),
         }
     }
 }
 
-impl TemplateApp {
+impl HollowApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -36,7 +37,7 @@ impl TemplateApp {
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for HollowApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
@@ -45,8 +46,6 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
-
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
         // Tip: a good default choice is to just keep the `CentralPanel`.
@@ -64,53 +63,60 @@ impl eframe::App for TemplateApp {
             });
         });
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let mut font_definitions = egui::FontDefinitions::default();
+            font_definitions.font_data.insert(
+                "NotoSans".to_owned(),
+                egui::FontData::from_static(include_bytes!("../assets/NotoSansJP-Regular.otf")),
+            );
+            font_definitions
+                .families
+                .insert(egui::FontFamily::Monospace, vec!["NotoSans".to_owned()]);
+            ctx.set_fonts(font_definitions);
+
+            ui.heading("The Corners Speak Latin");
 
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
+                ui.label("Normal Link:       ");
+                ui.text_edit_singleline(&mut self.normal_link);
             });
 
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
+            ui.horizontal(|ui| {
+                ui.label("Conspiracy Link: ");
+                ui.text_edit_singleline(&mut self.conspiracy_link);
+            });
+
+            let prompt = Prompt::new(&self.normal_link, &self.conspiracy_link, "ja");
+
+            if ui.button("SeEk Truth").clicked() {
+                let the_spooky = match prompt.run() {
+                    Ok(entry) => entry,
+                    Err(_) => "Error while converting".to_owned(),
+                };
+                self.output = format!("{}", the_spooky);
             }
 
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
-                });
+            ui.horizontal(|ui| {
+                ui.label("Output: ");
+                ui.add(
+                    egui::TextEdit::multiline(&mut self.output)
+                        .desired_width(ui.available_width())
+                        .desired_rows(20)
+                        .font(egui::TextStyle::Monospace)
+                        .frame(false),
+                );
             });
         });
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
-        });
-
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally choose either panels OR windows.");
-            });
-        }
+        // egui::CentralPanel::default().show(ctx, |ui| {
+        //     // The central panel the region left after adding TopPanel's and SidePanel's
+        //     ui.heading("eframe template");
+        //     ui.hyperlink("https://github.com/emilk/eframe_template");
+        //     ui.add(egui::github_link_file!(
+        //         "https://github.com/emilk/eframe_template/blob/master/",
+        //         "Source code."
+        //     ));
+        //     egui::warn_if_debug_build(ui);
+        // });
     }
 }
